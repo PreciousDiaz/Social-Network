5719ebc96b1bc0b1db2e6b241cfdb4968509bc6f
PreciousDiaz <elladiaz5@gmail.com>
2016-12-27 23:18:13 +0800

    creation of sign in and sign up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,47 @@
 @extends('layouts.master')
+
 @section('title')
-WELCOME! 
+	WELCOME! 
 @endsection
+
 @section('content')
-This is a content
+	<div class = "row">
+	<h3>Sign Up</h3>
+		<div class="col-6">
+			<form action="#" method="post">
+				<div class ="form-group">
+					<label form="email">Your E-mail</label>
+					<input class = "form-control" type="text" name="email" id="email">
+				</div>
+				
+				<div class ="form-group">
+					<label form="first_name">Your E-mail</label>
+					<input class = "form-control" type="text" name="first_name" id="first_name">
+				</div>
+				
+				<div class ="form-group">
+					<label form="password">Your Password</label>
+					<input class = "form-control" type="password" name="password" id="password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+	</div>
+	
+	<div class="col-6">
+	<h3>Sign In</h3>
+			<form action="#" method="post">
+				<div class ="form-group">
+					<label form="email">Your E-mail</label>
+					<input class = "form-control" type="text" name="email" id="email">
+				</div>
+			
+				<div class ="form-group">
+					<label form="password">Your Password</label>
+					<input class = "form-control" type="password" name="password" id="password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+	</div>
+</div>
+	
 @endsection
